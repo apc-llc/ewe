efd8a12784a2c58ddf5211d2d311f32524369f01
Mathias Winkel <win_wa@gmx.net>
2014-10-23 15:43:31 +0200

    fixed indexing error: Fortran starts with one, but who cares for Fortran - we are in C++here...

--- src/materials/CardiacWhiteley2007Material.C
+++ src/materials/CardiacWhiteley2007Material.C
@@ -38,12 +38,12 @@ CardiacWhiteley2007Material::CardiacWhiteley2007Material(const std::string  & na
 const RealTensorValue CardiacWhiteley2007Material::fromSymm(const SymmTensor & A)
 {
   RealTensorValue B;
+  B(0,0) = A(0,0);
   B(1,1) = A(1,1);
   B(2,2) = A(2,2);
-  B(3,3) = A(3,3);
+  B(0,1) = B(1,0) = A(0,1);
+  B(0,2) = B(2,0) = A(0,2);
   B(1,2) = B(2,1) = A(1,2);
-  B(1,3) = B(3,1) = A(1,3);
-  B(2,3) = B(3,2) = A(2,3);
   return B;
 }
 
