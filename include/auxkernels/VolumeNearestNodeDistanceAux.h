85a4548c6628a49a8799fb53fab1b11d3884fd2e
Mathias Winkel <win_wa@gmx.net>
2014-11-10 13:58:32 +0100

    documentation: added minor note on modified meshes in VolumeNearestNodeDistanceAux kernels

--- include/auxkernels/VolumeNearestNodeDistanceAux.h
+++ include/auxkernels/VolumeNearestNodeDistanceAux.h
@@ -19,6 +19,13 @@ InputParameters validParams<VolumeNearestNodeDistanceAux>();
  * In fact, most of the code here is taken from NearestNodeDistanceAux.h,
  * NearestNodeDistanceAux.C, and GeometricSearchData.C
  * and only had to be slightly modified to fit our needs.
+ *
+ * \todo TODO: Currently, all update functionality does not work.
+ * Accordingly, changes in the mesh configuration, displacements, etc.
+ * are ignored since findNodes() of the underlying
+ * VolumeNearestNodeDistance object is only invoked in the
+ * constructor. Refer to GeometricSearchData.C and call paths
+ * to this file to find out how it should really be done.
  */
 class VolumeNearestNodeDistanceAux : public AuxKernel
 {
