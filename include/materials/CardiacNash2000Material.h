63d6de0ede97940b960dcfbe5414375b5de59a52
Mathias Winkel <win_wa@gmx.net>
2014-12-10 10:39:50 +0100

    added modifications to CardiacNash2000Material as given by [Holzapfel, 2009] (some absolute values, specification of convexity check etc.)

--- include/materials/CardiacNash2000Material.h
+++ include/materials/CardiacNash2000Material.h
@@ -1,7 +1,7 @@
 #ifndef CardiacNash2000Material_H
 #define CardiacNash2000Material_H
 
-#include "CardiacMEchanicsMaterial.h"
+#include "CardiacMechanicsMaterial.h"
 #include "SymmTensor.h"
 
 class CardiacNash2000Material;
@@ -11,13 +11,11 @@ InputParameters validParams<CardiacNash2000Material>();
 
 /**
  * Implementation of the 'pole-free' hyperelastic material law
- * for cardiac tissue as given in [Nash 2000].
+ * for cardiac tissue as given in [Nash, 2000] with
+ * modifications and verifications as given by [Holzapfel, 2009].
  *
  * Parameters are read via MaterialParameters from the
- * input file. A linear material behavior (for debugging)
- * can be realized by setting the approriate \f$k_{ij}\f$ to
- * negative values, see CardiacNash2000Material::computeQpProperties()
- * for details.
+ * input file.
  */
 class CardiacNash2000Material : public CardiacMechanicsMaterial
 {
--- src/materials/CardiacNash2000Material.C
+++ src/materials/CardiacNash2000Material.C
@@ -1,10 +1,3 @@
-/****************************************************************/
-/*                                                              */
-/*  Implementation of Cardiac Tissue elasticity as given in     */
-/*  [Nash2000]                                                  */
-/*                                                              */
-/****************************************************************/
-
 #include "CardiacNash2000Material.h"
 #include "TensorHelpers.h"
 
@@ -27,6 +20,9 @@ CardiacNash2000Material::CardiacNash2000Material(const std::string  & name,
    _a(SymmTensor(getParam<std::vector<Real> >("a_MN"))),
    _b(SymmTensor(getParam<std::vector<Real> >("b_MN")))
 {
+  for (int M=0;M<3;M++)
+    for (int N=M;N<3;N++)
+       if (_k(M,N)<0 || _a(M,N)<=0 || _b(M,N)<=0) mooseError("CardiacNash2000Material: invalid material parameters.");
 }
 
 void
@@ -46,25 +42,17 @@ CardiacNash2000Material::computeQpStressProperties(const SymmTensor &E)
     for (int N=M;N<3;N++)
     {
       const Real k(_k(M,N));
-      const Real e(E(M,N));
+      const Real e(std::abs(E(M,N)));
+      const Real a(_a(M,N));
+      const Real b(_b(M,N));
+      if (b > 1 && b < 2 && e>a) mooseError("CardiacNash2000Material: E_{MN} >= a_{MN} - the strain is too large for this model");
+      const Real d(std::abs(a - e));
+      const Real f( b*e/d );
+      const Real g( k*pow(d,-b) );
 
-      if (k<0) {
-        // negative k values force a fallback to the linear case
-        T(M,N)  += -k;
-        D(M,N)  +=  0.;
-        _W[_qp] += -k*e;
-      } else if (k>0) {
-        const Real a(_a(M,N));
-        const Real b(_b(M,N));
-        const Real d( a - e );
-        if (d <= 0) mooseError("CardiacNash2000Material: E_{MN} >= a_{MN} - the strain is too large for this model");
-        const Real f( b*e/d );
-        const Real g( k*pow(d,-b) );
-
-        T(M,N) += 0.5 * g * e * ( 2+f );
-        D(M,N) += 0.5 * g * ( 2 + (4+e/d+f)*f );
-        W      += 0.5 * g * e*e;
-      }
+      T(M,N) += 0.5 * g * e * ( 2+f );
+      D(M,N) += 0.5 * g * ( 2 + (4+e/d+f)*f );
+      W      += 0.5 * g * e*e;
     }
 
   _stress[_qp] = STtoRTV(T);
