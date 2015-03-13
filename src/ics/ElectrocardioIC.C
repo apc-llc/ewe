45bf737fc2a1d3da11e12b5b507e6c9b3e287f22
Daniel Ruprecht <daniel.ruprecht@usi.ch>
2015-03-13 10:57:32 +0100

    modified resting potential for bernus model; apparently the ewe implementation does not exactly reproduce the -90.272mV; have to check parameters

--- src/ics/ElectrocardioIC.C
+++ src/ics/ElectrocardioIC.C
@@ -23,5 +23,6 @@ ElectrocardioIC::value(const Point & p)
 {
   /// The value -90.272 is the resting potential of the bernus model for all except one case...
   /// \todo TODO: make this more general: different ion models have different resting potentials
-  return -90.272;
+ // return -90.272;
+  return -92.189; // for some reason, my Bernus model has a slightly different resting potential.. probably a slighly wrong parameter?
 }
