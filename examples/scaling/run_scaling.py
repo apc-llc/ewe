cbb8e6f3a11d22cc92c73588bdab515e721c84e8
Daniel Ruprecht <daniel.ruprecht@usi.ch>
2015-03-19 11:00:15 +0100

    can now switch between normal and large run in python script

--- examples/scaling/run_scaling.py
+++ examples/scaling/run_scaling.py
@@ -1,11 +1,16 @@
 import os, math, numpy
 
 Nprocs = [1, 4, 8, 12, 16, 20, 24]
+largerun = False
 
 for i in range(0,numpy.size(Nprocs)):
   Nproc = Nprocs[i]
   with open("submit_ewe_Np"+str(Nproc)+".sh", "w") as myfile:
-    jobname = "ewe_Np"+str(Nproc)
+    if largerun:
+      jobname = "ewe_large_Np"+str(Nproc)
+    else:
+      jobname = "ewe_Np"+str(Nproc)
+    
     Nnodes = int(math.ceil(float(Nproc)/float(8.0)))
     myfile.write("#!/bin/bash \n")
     myfile.write("#SBATCH --job-name="+jobname+"\n")
@@ -18,6 +23,9 @@ for i in range(0,numpy.size(Nprocs)):
     myfile.write("echo JobID $SLURM_JOB_ID \n")
     myfile.write("echo Nodes $SLURM_NNODES \n")
     myfile.write("echo Tasks $SLURM_NTASKS \n")
-    myfile.write("/usr/bin/time mpirun -n "+str(Nproc)+" ../../ewe-opt --no-color -i coupled_bar.i \n")
+    if largerun:
+      myfile.write("/usr/bin/time mpirun -n "+str(Nproc)+" ../../ewe-opt --no-color -i coupled_bar_large.i \n")
+    else:
+      myfile.write("/usr/bin/time mpirun -n "+str(Nproc)+" ../../ewe-opt --no-color -i coupled_bar.i \n")
     myfile.close()
   os.system("sbatch submit_ewe_Np"+str(Nproc)+".sh")
