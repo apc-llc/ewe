b9a926da050d3e3558d6276d697f7c323e5d15f4
Daniel Ruprecht <daniel.ruprecht@usi.ch>
2015-03-19 10:16:45 +0100

    fixed node number in batch script

--- examples/scaling/run_scaling.py
+++ examples/scaling/run_scaling.py
@@ -6,10 +6,10 @@ for i in range(0,numpy.size(Nprocs)):
   Nproc = Nprocs[i]
   with open("submit_ewe_Np"+str(Nproc)+".sh", "w") as myfile:
     jobname = "ewe_Np"+str(Nproc)
-    Nnodes = math.ceil(Nproc/8)
+    Nnodes = int(math.ceil(float(Nproc)/float(8.0)))
     myfile.write("#!/bin/bash \n")
     myfile.write("#SBATCH --job-name="+jobname+"\n")
-    myfile.write("#SBATCH --nodes=1 \n")
+    myfile.write("#SBATCH --nodes="+str(Nnodes)+"\n")
     myfile.write("#SBATCH --ntasks="+str(Nproc)+"\n")
     myfile.write("#SBATCH --ntasks-per-node="+str(Nproc)+"\n")
     myfile.write("#SBATCH --cpus-per-task=1 \n")
